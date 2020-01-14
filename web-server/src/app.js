48bff6df4dbe7025fbffedfdc61983ea697dc8b6
Jeroen Walta <jeroenwalta@gmail.com>
2020-01-14 09:15:07 +0100

    q

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,17 +1,34 @@
 const path = require('path')
 const express = require('express')
-
+const hbs = require('hbs')
 
 const app = express()
+const viewsPath = path.join(__dirname, '../templates/views')
+const partialsPath = path.join(__dirname, '../templates/partials')
+
+app.set('view engine', 'hbs')
+app.set('views', viewsPath)
+hbs.registerPartials(partialsPath)
 
 
 app.use(express.static(path.join(__dirname, '../public')))
+
+app.get('', (req, res) => {
+    res.render('index', {
+        title: 'Weather App',
+        name: 'Jeroen'
+    })
+})
 
 app.get('/weather', (req, res) => {
     res.send({
         location: 'Den Haag',
         weather: 'It is snowing'
     })
+})
+
+app.get('*', (req, res) => {
+    res.send('404')
 })
 
 app.listen(3000, () => {
